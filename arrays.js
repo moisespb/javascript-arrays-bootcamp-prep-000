4443f628ca7a8a1da753a98ecd2d8c90e3fb3712
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-08-10 17:39:53 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,40 @@
+var chocolateBars = [
+  'snikers',
+  'hundred grand',
+  'kitkat',
+  'skittles'];
+  
+function addElementToBeginningOfArray(array, string)
+  {
+    var elements = ["element1","element2" ]
+    element.unshift("element3") 
+    return addElementToBeginningOfArray
+    
+  }
+function destructivelyAddElementToBeginningOfArray(array, element){
+    return destructivelyAddElementToBeginningOfArray()
+  }
+  
+function accessElementInArray(){
+    return accessElementInArray
+  }
+  
+function destructivelyRemoveElementFromBeginningOfArray(){
+    var cats = ["Milo", "Garfield", "Otis"]
+    cats.slice(1) 
+    return destructivelyRemoveElementFromBeginningOfArray
+  }
+  
+  
+function destructivelyRemoveElementFromBeginningOfArray(){
+    var cats = ["Milo", "Garfield", "Otis"]
+    
+    return destructivelyRemoveElementFromBeginningOfArray
+  }
+  
+    
+function destructivelyRemoveElementFromBeginningOfArray(){
+  var iceCreams = ["chocolate", "vanilla", "raspberry"]
+  iceCreams.pop() 
+  return destructivelyRemoveElementFromBeginningOfArray
+  }
